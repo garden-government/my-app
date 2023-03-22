15fca425f0b9b994cde02e9a5665192d09196f1e
Nikita_Bog <gardengovernment@mail.ru>
2023-03-22 21:14:20 +0700

    Основа

--- src/App.js
+++ src/App.js
@@ -1,39 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import ButtonFunctional from './Components/ButtonFunctional';
 import RangeFunctional from './Components/RangeFunctional';
 
-
-
 const test  = () => {
-  console.log(123423542434);
+  console.log(1234);
+}
+
+const loadFile = () => {
+  let el = document.getElementById("loadInputFile");
+  let file = el.files[0];
+  let reader = new FileReader();
+  reader.onload = function(){
+    loadImage(reader.result, "mainCanvas");
+    loadImage(reader.result, "helpCanvas");
+  }
+  reader.readAsDataURL(file);
+}
+
+const loadFile2 = () => {
+  let el = document.getElementById("loadInputFile2");
+  let file = el.files[0];
+  let reader = new FileReader();
+  reader.onload = function(){
+    loadImage(reader.result, "helpCanvas");
+  }
+  reader.readAsDataURL(file);
+}
+
+function loadImage(srcImage, idCanvas){
+  const img = new Image();
+  img.src = srcImage;
+  
+  img.onload = function () {
+    const can = document.getElementById(idCanvas);
+
+    const ctx = can.getContext("2d");
+    can.width = img.width;
+    can.height = img.height;
+
+    ctx.drawImage(img,0,0,can.width,can.height);
+  }
+}
+
+function sliderRed (event) {
+
+  const canMain = document.getElementById("mainCanvas");
+  const ctxMain = canMain.getContext("2d");
+  const scane = ctxMain.getImageData(0,0,canMain.width,canMain.height); 
+
+  const canHelp = document.getElementById("helpCanvas");
+  const ctxHelp = canHelp.getContext("2d");
+  const scaneHelp = ctxHelp.getImageData(0,0,canHelp.width,canHelp.height);
+
+  let valueForChange = event.target.value;
+  
+  if(canMain.width === canHelp.width && canMain.height === canHelp.height){
+
+    for (let i  = 0; i < scane.data.length;i+=4){
+      scane.data[i] = scaneHelp.data[i] * Number(valueForChange) / 100;
+    } 
+    console.log( Number(valueForChange))
+  }
+
+  ctxMain.putImageData(scane,0,0);
 }
 
 function App() {
-  const img = new Image();
-  const nothingDoing = () => {};
-
-
-  img.onload = function () {
-    const can = document.getElementById("c");
-    can.width = can.height = 500;
-    const ctx = can.getContext("2d");
-
-    //ctx.drawImage(img,0,0,can.width,can.height);
-  
-    const scane = ctx.getImageData(0,0,can.width,can.height);
-
-    for (let i  = 0; i < scane.data.length;i+=4){
-      scane.data[i] = 244; //changeLastBit(scanData[i], scanData2[i], howBit)
-      scane.data[i+1] = 200; //changeLastBit(scanData[i+1], scanData2[i+1], howBit)
-      scane.data[i+2] = 150; //changeLastBit(scanData[i+2], scanData2[i+2], howBit)
-      scane.data[i+3] = 170;
-    }
-    
-    ctx.putImageData(scane,0,0);
-  }
-
-
 
   
 
@@ -61,33 +94,20 @@ function App() {
 
         <div className='LoadImage'>
           <div>
-            <a target="_blank" href="https://snipp.ru/tools/base64-img">
-              <label>Link</label>              
-            </a>
-            <input type= "text" name= "loadInput" placeholder="data:image/png;base64..."></input>            
+            <input type= "file" id= "loadInputFile" onChange={loadFile}></input>           
           </div>
           
           <ButtonFunctional idv = "loadImage" title = "Load" functional={test}/>
         </div>
 
         <div className='Sliders'>
-          <RangeFunctional id = "Separia" max={100} min={0} defoult={0} title = "Separia"  functional={test}></RangeFunctional>
+          <RangeFunctional id = "Red" max={30} min={0} defoult={10} title = "Separia"  functional={sliderRed}></RangeFunctional>
         </div>
 
         <div className='WriteImage'>
 
           <div>
-            <a target="_blank" href="https://snipp.ru/tools/base64-img">
-              <label>Image 1</label>             
-            </a>
-            <input type= "text" name= "loadImage1" placeholder="data:image/png;base64..."></input>
-          </div>
-
-          <div>
-            <a target="_blank" href="https://snipp.ru/tools/base64-img">
-              <label>Image 2</label>             
-            </a>
-            <input type= "text" name= "loadImage2" placeholder="data:image/png;base64..."></input>            
+            <input type= "file" id= "loadInputFile2" onChange={loadFile2}></input>
           </div>
 
           <div>
--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-class CanvasComponent extends React.Component {
-  componentDidMount() {
-      this.updateCanvas();
-  }
-  updateCanvas() {
-      const ctx = this.refs.canvas.getContext('2d');
-      ctx.fillRect(0,0, 100, 100);
-  }
-  updateCanvasButton() {
-    const ctx = this.refs.canvas.getContext('2d');
-    ctx.fillRect(0,0, 200, 200);
-}
-  render() {
-      return (
-          <canvas id="canvas" ref="canvas" width={300} height={300}/>
-      );
-  }
-}
-
-//ReactDOM.render(<CanvasComponent/>, document.getElementById('canv'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
